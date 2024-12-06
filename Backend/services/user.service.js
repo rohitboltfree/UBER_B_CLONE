a4bffe46e524cc18b0c60a77cb8ac43e7123fa4b
rohitboltfree <rohitk192081@gmail.com>
2024-12-06 10:06:54 +0530

    routes updated

---
+++ Backend/services/user.service.js
@@ -0,0 +1,21 @@
+const userModel = require('../models/user.model');
+
+// for to create user we to set the required field from the schema 
+module.exports.createUser = async ({
+    //accept these as a object 
+        firstname,lastname,email,password
+}) =>{
+    if(!firstname || !email || !password){
+        throw new Error('All field are required');
+    }
+    const user = userModel.create({
+        fullName:{
+            firstname,
+            lastname
+        },
+        email,
+        password
+    })
+
+    return user;
+}
