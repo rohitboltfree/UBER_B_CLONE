9d469ca6ee3eb6d33b44a1e77960dc22e7f80f2a
rohit <rohitk192081@gmail.com>
2024-12-27 09:45:14 +0530

    login page ui is created for user

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <div className='h-screen pt-8  w-full flex justify-between flex-col bg-red-400 '>
+      <div className='bg-top bg-cover bg-[url(/images/bg-cover.jpg)] h-screen pt-8  w-full flex justify-between flex-col bg-red-400 '>
       <img className='w-14 ml-8' src="/images/uber-img.png" alt="uber-image" />
         <div className='bg-white pb-7 py-4 px-4 '>
           <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
-          <button className='w-full bg-black text-white py-3 rounded mt-6'>Continue</button>
+          <Link to='/login' className='flex justify-center items-center w-full bg-black text-white py-3 rounded mt-6'>Continue</Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userData, setUserData] = useState({});
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    
+    setUserData({
+      email:email,
+      password:password
+    })
+
+    console.log(userData);
+    setEmail('')
+    setPassword('')
+  }
+
   return (
-    <div>
-      user-l
+    <div className='p-7  h-screen flex flex-col justify-between'>
+      <div>
+        <img className='w-14  mb-7' src="/images/uber-img.png" alt="uber-image" />
+        <form onSubmit={(e)=>{
+          submitHandler(e)
+        } }>
+          <h3 className='text-xl mb-2'>What's your email</h3>
+          <input 
+            value={email}
+            onChange={(e)=>{
+              setEmail(e.target.value)
+            }}
+          className='bg-[#eeeeee] rounded px-4 py-2 border w-full text-lg mb-7 placeholder:text-base ' required type="email" placeholder='email@youremail.com' />
+          <h3 className='text-xl mb-2'>Enter Password</h3>
+          <input 
+            value={password}
+            onChange={(e)=>{
+              setPassword(e.target.value)
+            }}
+
+          className='bg-[#eeeeee] rounded px-4 py-2 border w-full mb-7 text-lg placeholder:text-base ' required type="password" placeholder='enter your password' />
+          <button className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 border w-full text-lg placeholder:text-base ' >Login</button>
+          <div className='flex w-full justify-center'>
+            <p className='text-center'>If You Are New ? </p>
+            <Link to='/signup' className='text-blue-600'>Create New Accout</Link>
+          </div>
+        </form>
+
+      </div>
+      <div>
+         <Link to='/captain-login' className='bg-[#10b461] text-white font-semibold flex justify-center items-center mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base '>Sign as captain</Link>
+          </div>
     </div>
   )
 }
