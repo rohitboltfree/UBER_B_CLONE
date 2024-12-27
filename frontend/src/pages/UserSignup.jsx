a10bd5b379e56587add08235435fbf18fe9bc69b
rohit <rohitk192081@gmail.com>
2024-12-27 12:57:29 +0530

    signup page form and ui has been created

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const UserSignup = () => {
+
+  const [email,setEmail] = useState('');
+  const [password, setPassword]= useState('');
+  const [firstName, setFirstName]= useState('');
+  const [lastName, setLastName]= useState('');
+  const [userData, setUserData]  = useState({});
+
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    
+    setUserData({
+      userName:{
+        firstName:firstName,
+        lastName:lastName
+      },
+      email:email,
+      password:password
+    })
+   
+    console.log(userData);
+    setEmail('');
+    setFirstName('');
+    setLastName('');
+    setPassword('');
+  }
+
   return (
-    <div>
-      user-sp
+    <div className='p-7  h-screen flex flex-col justify-between'>
+      <div>
+        <img className='w-14  mb-5' src="/images/uber-img.png" alt="uber-image" />
+        <form onSubmit={(e)=>{
+          submitHandler(e)
+        } } >
+          <h3 className='text-base font-medium mb-2'>What's your Name</h3>
+          <div className='flex gap-4 mb-5'>
+            <input
+              value={firstName}
+              onChange={(e)=>{
+                setFirstName(e.target.value)
+              }}
+              className='bg-[#eeeeee] rounded px-4 py-2 border w-1/2 text-sm placeholder:text-sm ' required type="text" placeholder='Firstname' />
+            <input
+              value={lastName}
+              onChange={(e)=>{
+                setLastName(e.target.value)
+              }}
+              className='bg-[#eeeeee] rounded px-4 py-2 border w-1/2 text-sm placeholder:text-sm ' required type="text" placeholder='Lastname' />
+          </div>
+
+          <h3 className='text-base font-medium mb-2'>What's your email</h3>
+          <input
+            value={email}
+            onChange={(e)=>{
+              setEmail(e.target.value)
+            }}
+            className='bg-[#eeeeee] rounded px-4 py-2 border w-full text-sm mb-5 placeholder:text-sm ' required type="email" placeholder='email@youremail.com' />
+
+          <h3 className='text-base font-medium mb-2'>Enter Password</h3>
+          <input
+          value={password}
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }}
+            className='bg-[#eeeeee] rounded px-4 py-2 border w-full mb-7 text-lg placeholder:text-sm ' required type="password" placeholder='enter your password' />
+
+          <button className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 border w-full text-sm placeholder:text-base ' >SignUp</button>
+          <div className='flex w-full justify-center'>
+            <p className='text-center'>Already have account ?   </p>
+            <Link to='/login' className='text-blue-600'> Sign-In</Link>
+          </div>
+        </form>
+
+      </div>
+      <div>
+        <p className='text-[12px] leading-tight '> By processing, you consent to get calls, WhatsApp or SMS messages, including by automated bmeans, from Uber and its affiliates to the number provided. </p>
+      </div>
     </div>
   )
 }
